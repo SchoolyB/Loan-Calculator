b68fa085c75f3cd6ce031804070a5e219f1cdff3
SchoolyB <110133622+SchoolyB@users.noreply.github.com>
2022-07-28 10:13:59 -0500

    Add files via upload

---
+++ Loan Calculator1.py
@@ -0,0 +1,13 @@
+L = input("How much are you looking to borrow")
+i = input("what is an interest rate you can afford?")
+n = input('how many payments can you make?')
+
+L = float(L)
+i = float(i)
+n = float(n)
+
+numberOfPayments = n * 12
+
+M = L * i * (1+i) * n / (1+i) * n - 1
+
+print('Your monthly payment is' + M)
